4130b7ac6fc599c6953c1e0d4aac2a3f4b50d0fe
KingZuko <rs6121@nyu.edu>
2023-02-06 16:21:34 -0500

    Basic infrastrucutre

--- src/client/index.js
+++ src/client/index.js
@@ -1,42 +1,18 @@
-// Learn more about this file at:
-// https://victorzhou.com/blog/build-an-io-game-part-1/#3-client-entrypoints
-import { connect, play } from './networking';
-import { startRendering, stopRendering } from './render';
-import { startCapturingInput, stopCapturingInput } from './input';
-import { downloadAssets } from './assets';
-import { initState } from './state';
-import { setLeaderboardHidden } from './leaderboard';
-
-// I'm using a tiny subset of Bootstrap here for convenience - there's some wasted CSS,
-// but not much. In general, you should be careful using Bootstrap because it makes it
-// easy to unnecessarily bloat your site.
-import './css/bootstrap-reboot.css';
-import './css/main.css';
+// All of our js logic comes together here
+// import {} from ./networking
+// 8 of these sort of things
 
 const playMenu = document.getElementById('play-menu');
 const playButton = document.getElementById('play-button');
-const usernameInput = document.getElementById('username-input');
+const getUserButton = document.getElementById('user-input');
 
-Promise.all([
-  connect(onGameOver),
-  downloadAssets(),
-]).then(() => {
-  playMenu.classList.remove('hidden');
-  usernameInput.focus();
-  playButton.onclick = () => {
-    // Play!
-    play(usernameInput.value);
-    playMenu.classList.add('hidden');
-    initState();
-    startCapturingInput();
-    startRendering();
-    setLeaderboardHidden(false);
-  };
-}).catch(console.error);
+// then we have all these other client side javascript logic
 
-function onGameOver() {
-  stopCapturingInput();
-  stopRendering();
-  playMenu.classList.remove('hidden');
-  setLeaderboardHidden(true);
-}
+// networking, rendering, setLeaderboard, caputringInput,initState
+
+// click leads from game state 0 to game state 1
+
+// so lets get about writing our networking, rendering, etc..
+
+
+
