be61a394f8a993e3e2d0f064255093df70be6d4f
KingZuko <rs6121@nyu.edu>
2023-02-15 09:38:47 -0500

    adding all the changes since deleting everything

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,8 @@ const playMenu = document.getElementById('play-menu');
 const playButton = document.getElementById('play-button');
 const getUserButton = document.getElementById('user-input');
 
+import './css/bootstrap-reboot.css';
+import './css/main.css';
 // then we have all these other client side javascript logic
 
 // networking, rendering, setLeaderboard, caputringInput,initState
---
+++ src/client/networking.js
@@ -0,0 +1,4 @@
+// ok lets write the networking code for the client side
+// It would probably require me to do some socket io client side stuff?
+
+const socket
